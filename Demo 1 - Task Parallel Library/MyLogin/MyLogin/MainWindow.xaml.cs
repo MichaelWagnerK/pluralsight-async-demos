5da9759962657cb59ab75564e7d5526ec4178cb3
@MichaelWagnerK <mwagner@resdat.com>
2017-08-04 10:59:07 -0500

    Initial load

---
+++ Demo 1 - Task Parallel Library/MyLogin/MyLogin/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Ink;
+
+namespace MyLogin
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LoginButton.IsEnabled = false;
+                BusyIndicator.Visibility = Visibility.Visible;
+
+                var result = await LoginAsync();
+
+                LoginButton.Content = result;
+            }
+            catch (Exception)
+            {
+                LoginButton.Content = "Login Failed!";
+            }
+            finally
+            {
+                LoginButton.IsEnabled = true;
+                BusyIndicator.Visibility = Visibility.Hidden;
+            }
+
+
+        }
+
+        private async Task<string> LoginAsync()
+        {
+            try
+            {
+                var loginTask = await Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+
+                    return "Login Successful!";
+                });
+
+                return loginTask;
+            }
+            catch (Exception)
+            {
+                return "Login Failed!";
+            }
+        }
+
+
+        //private void LoginButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    LoginButton.IsEnabled = false;
+        //    var task = Task.Run(() => {
+
+        //        Thread.Sleep(2000);
+
+        //        return "Login Successful!";
+        //    });
+
+        //    //task.ContinueWith(t => {
+        //    //    if (t.IsFaulted)
+        //    //    {
+        //    //        Dispatcher.Invoke(() =>
+        //    //        {
+        //    //            LoginButton.Content = "Login failed!";
+        //    //            LoginButton.IsEnabled = true;
+        //    //        });
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        Dispatcher.Invoke(() =>
+        //    //        {
+        //    //            LoginButton.Content = t.Result;
+        //    //            LoginButton.IsEnabled = true;
+        //    //        });
+        //    //    }
+        //    //});
+
+        //    task.ConfigureAwait(true)
+        //        .GetAwaiter()
+        //        .OnCompleted(() =>
+        //        {
+        //            LoginButton.Content = task.Result;
+        //            LoginButton.IsEnabled = true;
+        //        });
+
+        //}
+    }
+}
---
+++ TplAsyncSandbox/SandboxConsole/SandboxConsole/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SandboxConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            //string title = GetTitleTplAsync("a").Result;
+
+            //Console.WriteLine(title);
+
+            //DoWorkAsync("a");
+
+            //Thread.Sleep(500);
+
+            //DoWorkAsync("b");
+
+
+            //CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            //CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            //var str = TestCancelAsync(cancellationToken);
+
+            //do
+            //{
+            //    var c = Console.ReadLine();
+            //    Console.WriteLine(c);
+            //    if (c != "c") continue;
+            //    cancellationTokenSource.Cancel();
+            //} while (true);
+
+            CancellationDemo1();
+
+            Console.ReadLine();
+        }
+
+        private static void CancellationDemo1()
+        {
+            using (CancellationTokenSource cancellationTokenSource = new CancellationTokenSource())
+            {
+                var waitFor = new List<Task>();
+
+                CancellationToken cancellationToken = cancellationTokenSource.Token;
+                Action<Task<int>> continuation = t =>
+                {
+                    if (t.IsCanceled)
+                    {
+                        Console.WriteLine("Task was cancelled");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Status " + t.Result + ": " + t.Status);
+                    }
+                };
+
+                for (int i = 0; i < 100; i++)
+                {
+                    int current = i;
+                    var c = Task.Factory
+                        .StartNew(() =>
+                            {
+                                Console.WriteLine("Running " + current);
+                                Thread.Sleep(100);
+                                Console.WriteLine("Finishing " + current);
+                                return current;
+                            }, cancellationToken)
+                            .ContinueWith(continuation);
+                    Console.WriteLine("Launched " + current);
+                    waitFor.Add(c);
+
+                }
+                Console.WriteLine("Waiting....");
+                Thread.Sleep(500);
+
+                Console.WriteLine("Cancelling....");
+                cancellationTokenSource.Cancel();
+                Console.WriteLine("Cancel Returned");
+                Task.WaitAll(waitFor.ToArray());
+            }
+        }
+
+        private static async Task TestCancelAsync(CancellationToken cancellationToken)
+        {
+            do
+            {
+                Func<string> funcWaiting = WaitingToCancel;
+                var task = await Task.Run(funcWaiting, cancellationToken);
+
+                Console.WriteLine(task);
+
+            } while (!cancellationToken.IsCancellationRequested);
+
+            Console.WriteLine("Cancellation requested: " + cancellationToken.IsCancellationRequested);
+        }
+
+        private static string WaitingToCancel()
+        {
+            Thread.Sleep(2000);
+            return DateTime.Now.ToLongTimeString();
+        }
+
+
+        private static async Task<string> GetTitleCsAsync(string url)
+        {
+            var task = await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return "url: " + DateTime.Now.ToLongTimeString();
+            });
+
+            return task;
+        }
+
+        private static Task<string> GetTitleTplAsync(string url)
+        {
+            var task = Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return "url: " + DateTime.Now.ToLongTimeString();
+            });
+
+            return task;
+        }
+
+
+        private static void DoWork()
+        {
+            Task<string> task = GetString();
+            task.ContinueWith(t =>
+            {
+                string result = t.Result;
+                Console.WriteLine(result);
+            });
+
+            Console.WriteLine("Work Done");
+
+        }
+
+        private static async void DoWorkAsync(string id)
+        {
+            Console.WriteLine("Starting Work: " + id);
+            Task<string> task = GetString();
+            string result = await task;
+            Console.WriteLine(result);
+            Console.WriteLine("Work Done: " + id);
+        }
+
+        private static async Task<string> GetString()
+        {
+            var task = await Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+
+                return "String From Task";
+            });
+
+            return task;
+        }
+    }
+}
